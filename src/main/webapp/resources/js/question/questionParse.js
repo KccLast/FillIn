c967641e16d2f74c29bb3bfa60fc2ba9174b708d
mimi5963 <mimi5963@naver.com>
2024-11-12 05:54:57 +0900

    캐싱 적용

--- src/main/webapp/resources/js/question/questionParse.js
+++ src/main/webapp/resources/js/question/questionParse.js
@@ -35,7 +35,10 @@ async function processQuestions(questions) {
     await appendQuestionCard(question, index); // 비동기 함수가 순차적으로 실행되도록 처리
   }
 }
-
+// 캐시 객체를 사용하여 AJAX 요청 최소화
+let questionFrameCache,
+  headerCache = {},
+  contentCache = {};
 // async function processQuestions(questions) {
 //   const questionCards = []; // 모든 질문 카드를 저장할 배열
 
@@ -294,6 +297,21 @@ async function fetchQuestionItem(seq) {
     type: 'GET',
   });
 }
+let questionItemCache = {};
+async function fetchQuestionItem(seq) {
+  if (!questionItemCache[seq]) {
+    try {
+      questionItemCache[seq] = await $.ajax({
+        url: '/resources/html/question/questionItem/qi' + seq + '.html',
+        type: 'GET',
+      });
+    } catch (error) {
+      console.error(`Question item 불러오기 실패 (seq: ${seq}):`, error);
+      questionItemCache[seq] = null;
+    }
+  }
+  return questionItemCache[seq];
+}
 
 function type7Common(target, qi) {
   target.find('.j-select-optionBox').addClass(qi.seq + ' ' + qi.questionSeq);
@@ -320,25 +338,70 @@ function type9Common(target, qi) {}
 
 /** 질문 업데이트 관련 제목, 내용, 타입이 수정될 수 있음 */
 // questionFrame.html 파일 불러오기
+
+// async function fetchQuestionFrame() {
+//   return $.ajax({
+//     url: '/resources/html/question/questionFrame.html',
+//     type: 'GET',
+//   });
+// }
 async function fetchQuestionFrame() {
-  return $.ajax({
-    url: '/resources/html/question/questionFrame.html',
-    type: 'GET',
-  });
+  if (!questionFrameCache) {
+    try {
+      questionFrameCache = await $.ajax({
+        url: '/resources/html/question/questionFrame.html',
+        type: 'GET',
+      });
+    } catch (error) {
+      console.error('Question frame 불러오기 실패:', error);
+      questionFrameCache = null;
+    }
+  }
+  return questionFrameCache;
 }
 
 // header 파일 불러오기
+// async function fetchHeader(ccSeq) {
+//   return $.ajax({
+//     url: '/resources/html/question/header/header' + ccSeq + '.html',
+//     type: 'GET',
+//   });
+// }
+
 async function fetchHeader(ccSeq) {
-  return $.ajax({
-    url: '/resources/html/question/header/header' + ccSeq + '.html',
-    type: 'GET',
-  });
+  if (!headerCache[ccSeq]) {
+    try {
+      headerCache[ccSeq] = await $.ajax({
+        url: `/resources/html/question/header/header${ccSeq}.html`,
+        type: 'GET',
+      });
+    } catch (error) {
+      console.error(`Header 불러오기 실패 (ccSeq: ${ccSeq}):`, error);
+      headerCache[ccSeq] = null;
+    }
+  }
+  return headerCache[ccSeq];
 }
 
 // content 파일 불러오기
+// async function fetchContent(ccSeq) {
+//   return $.ajax({
+//     url: '/resources/html/question/content/content' + ccSeq + '.html',
+//     type: 'GET',
+//   });
+// }
+
 async function fetchContent(ccSeq) {
-  return $.ajax({
-    url: '/resources/html/question/content/content' + ccSeq + '.html',
-    type: 'GET',
-  });
+  if (!contentCache[ccSeq]) {
+    try {
+      contentCache[ccSeq] = await $.ajax({
+        url: `/resources/html/question/content/content${ccSeq}.html`,
+        type: 'GET',
+      });
+    } catch (error) {
+      console.error(`Content 불러오기 실패 (ccSeq: ${ccSeq}):`, error);
+      contentCache[ccSeq] = null;
+    }
+  }
+  return contentCache[ccSeq];
 }
